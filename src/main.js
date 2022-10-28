8323ad2cd5bee38d83d01c230ca613af22b58c6e
nigulasi <nigulasi@qq.com>
2022-10-28 16:02:34 +0800

    引入字体图标，引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 // 引入全局样式
 import './styles/index.less'
-
+// 引入 vant组件库
+import Vant from 'vant'
+// 引入Vant组件样式
+import 'vant/lib/index.css'
+// 注册使用 Vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
